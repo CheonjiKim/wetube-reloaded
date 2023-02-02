3966155aaacca205b16ce1fe80791f1c4429d522
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-02-02 21:36:53 +0900

    #3.4
    3.4까지 강의 시청 후 코드 작성함.

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("Hello world");
+};
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
-
-app.get("/", () => console.log("Somebody is trying to go home."));
 
 app.listen(PORT, handleListening);
