fe01562d2f7b6f40116564cda000571c4b37a480
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-02-12 02:22:30 +0900

    #4.3
    4.3강까지 강의를 시청하였다.
    
    강의에서 나온 코드(router, controller)의 구현은 아직 하지 않은 상태이다.

--- src/server.js
+++ src/server.js
@@ -4,13 +4,12 @@ import morgan from "morgan";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
+app.use(logger); // 이 코드가 다른 app.use()보다 상단에 위치해야만 morgan()이 제대로 동작한다.
 
-const handleHome = (req, res) => {
-  return res.send("Hello world");
-};
-
-app.use(logger);
-app.get("/", handleHome);
+const userRouter = express.Router();
+const removeUserCon = (req, res) => res.send("remove user");
+app.use("/user", userRouter);
+userRouter.get("/remove", removeUserCon);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
