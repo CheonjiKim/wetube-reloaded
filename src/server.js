1951457d776111b4be007a6a363cf7b989baac20
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-02-11 15:57:59 +0900

    #3.11
    3.11까지의 강의를 시청함

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("Hello world");
 };
 
+app.use(logger);
 app.get("/", handleHome);
 
 const handleListening = () =>
