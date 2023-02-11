f8853b5ecb7e100d3af16f30e54cc093068dd5ca
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-02-12 08:15:35 +0900

    #4.8
    4.8까지 강의를 시청하고 강의 내용의 개념을 구현까지 해보았다.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 app.use(logger); // 이 코드가 다른 app.use()보다 상단에 위치해야만 morgan()이 제대로 동작한다.
 
-const userRouter = express.Router();
-const removeUserCon = (req, res) => res.send("remove user");
+app.use("/", globalRouter);
 app.use("/user", userRouter);
-userRouter.get("/remove", removeUserCon);
+app.use("/video", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
