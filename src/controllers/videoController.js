6e5206784dc2e711926bdec29ead50cab33f677b
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-04-28 15:44:40 +0900

    5.6
    5.6까지 강의를 시청하고, 구현까지 마쳤다.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,8 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send(`see Video #${req.params.id}`);
+export const trending = (req, res) => res.render("home", {pageTitle: "Comes from your controller", text: "the homepage of this website"});
+
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete video");
+
--- src/server.js
+++ src/server.js
@@ -7,11 +7,13 @@ import videoRouter from "./routers/videoRouter";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views")
 app.use(logger); // 이 코드가 다른 app.use()보다 상단에 위치해야만 morgan()이 제대로 동작한다.
 
 app.use("/", globalRouter);
 app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
