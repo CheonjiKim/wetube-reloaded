80c89cbe5d0506e8de51ad4dba77d55ece82feb0
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-04-29 10:23:27 +0900

    5.10
    5.10까지 강의 시청 후 구현까지 완료했다.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,38 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Comes from your controller", text: "the homepage of this website"});
+const videos = [
+  {
+    title: "vid1",
+    rating: 5,
+    comments: 10,
+    createdAt: "21 minutes ago",
+    views: 12334,
+    id: 0,
+  },
+  {
+    title: "vid2",
+    rating: 4.5,
+    comments: 130,
+    createdAt: "2 minutes ago",
+    views: 1234,
+    id: 1,
+  },
+  {
+    title: "vid3",
+    rating: 2,
+    comments: 10,
+    createdAt: "23 minutes ago",
+    views: 0,
+    id: 2,
+  },
+];
+
+export const trending = (req, res) =>
+  res.render("home", {
+    pageTitle: "Comes from your controller",
+    videos: videos,
+  });
 
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete video");
-
