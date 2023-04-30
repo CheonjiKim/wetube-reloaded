1de35d8abc7f3958d63a8b08bbf1dc071937dec2
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-04-30 14:58:14 +0900

    6.6
    6.6까지 강의를 시청하고 구현까지 완료했다.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,4 +45,28 @@ export const getEdit = (req, res) => {
 
   res.render("Edit", { pageTitle: `Editing: ${video.title}`, video: video });
 };
-export const postEdit = (req, res) => {};
+export const postEdit = (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  console.log(title);
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", {pageTitle: "Upload a Video"});
+};
+
+export const postUpload = (req, res) => {
+  const {title} = req.body;
+  const newVideo = {
+    title: title,
+    rating: 0,
+    comments: 0,
+    createdAt: "Just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/server.js
+++ src/server.js
@@ -8,11 +8,11 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views")
+app.set("views", process.cwd() + "/src/views"); 
 app.use(logger); // 이 코드가 다른 app.use()보다 상단에 위치해야만 morgan()이 제대로 동작한다.
-
+app.use(express.urlencoded({extended: true})); // This code makes Express understand HTML forms and values.
 app.use("/", globalRouter);
-app.use("/user", userRouter);
+app.use("/user", userRouter);  
 app.use("/videos", videoRouter);
 
 const handleListening = () =>
