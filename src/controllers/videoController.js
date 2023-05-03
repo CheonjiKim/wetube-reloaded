4dfff151c27a67f7289f47a4a987a502de13cd9e
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-05-03 11:48:44 +0900

    6.13
    6.13까지 강의를 보고 코드까지 따라 구현함

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,28 @@
+import Video from "../models/Video";
 
-export const home = (req, res) =>
-  res.render("home", {
-    pageTitle: "Comes from your controller",
-  });
+
+/*
+// 6.11 에서 Model.find()함수를 callback을 이용하여 구현하려고 했으나
+// 더 이상 callback을 지원하지 않는다고 한다.
+// 다음 코드는 callback을 사용한 코드이다. 
+export const home = (req, res) =>{
+  Video.find({}, (error, videos) =>{
+    console.log("errors", error);
+    console.log("videos", videos);
+  })
+  return res.render("home", {pageTitle: "Home", videos: videos})
+};
+*/
+export const home = async (req, res) => {
+  try {
+    console.log("First");
+    const videos = await Video.find({});
+    console.log("Second");
+    return res.render("home", {pageTitle: "Home", videos: videos});
+  } catch {
+    return res.render("server-error");
+  }
+};
 
 export const watch = (req, res) => {
   const id = req.params.id; // another equivalent code for this line -> const { id } = req.params;
