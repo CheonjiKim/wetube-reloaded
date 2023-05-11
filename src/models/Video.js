566eb0fe1c74fc8f15c63bfdcc99a93ce8ee32c0
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-05-11 15:46:46 +0900

    6.18
    6.18까지 강의를 시청하고 구현까지 마쳤다.

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 // 우리가 다룰 영상의 형태를 미리 구성해놓은 것이다.
 // Java의 Interface와 비슷한 개념인것 같다.
 const videoSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
+    title: {type: String, required: true, trim: true, maxLength: 80},
+    description: {type: String, required: true, trim: true, minLength: 20},
     createdAt: {type: Date, required: true, default: Date.now},
     hashtags: [{type: String}],
     meta: {
