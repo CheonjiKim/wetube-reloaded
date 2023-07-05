6634acb412dc53cb39bd6036b0506e5b132d6a62
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-07-05 16:02:08 +0900

    7.6
    7.6까지 영상 시청 후 구현까지 함

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
 export const postJoin = async (req, res) => {
@@ -18,7 +19,8 @@ export const postJoin = async (req, res) => {
             pageTitle,
             errorMessage: "The username/email is already taken."});
     }
-    await User.create({
+    try {
+        await User.create({
         name,
         email,
         username,
@@ -26,10 +28,32 @@ export const postJoin = async (req, res) => {
         location,   
     });
     return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", {
+          pageTitle: "Login",
+          errorMessage: error._message,
+        });
+      }
 };
+
+export const postLogin = async (req, res) => {
+    const {username, password} = req.body;
+    const pageTitle = "Login";
+    const user = await User.findOne({username});
+    if (!user) {
+        return res.status(400).render("login", { pageTitle, errorMessage: "The username does not exist."});
+    }
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok) {
+        return res.status(400).render("login", { pageTitle, errorMessage: "Incorrect password."});
+    }
+    console.log("Log user in!");
+    return res.redirect("/");
+}
+
+export const getLogin = (req, res) => res.render("login", {pageTitle: "Login"});
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See user");
