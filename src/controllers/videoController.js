965c247119c27153fc911ce52d8f58cdbc4234b6
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-07-16 19:32:44 +0900

    8.13
    8.13까지 시청 후 구현까지 함

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -59,7 +59,7 @@ export const postUpload = async (req, res) => {
   const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
-    await Video.create({
+    const newVideo = await Video.create({
       title,
       description,
       fileUrl,
@@ -67,6 +67,9 @@ export const postUpload = async (req, res) => {
       createdAt: Date.now(),
       hashtags: Video.formatHashtags(hashtags),
     });
+    const user = await User.findById(_id);
+    user.videos.push(newVideo._id);
+    user.save();
     return res.redirect("/");
   } catch (error) {
     return res.status(400).render("upload", {
