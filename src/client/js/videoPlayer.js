248a8fc990765b19201378966b00ab025293ec8f
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-07-19 14:53:05 +0900

    11.1
    11.1까지 영상 시청 후 구현까지 완료함

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,23 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) video.play();
+  else video.pause();
+};
+const handlePlay = (e) => {
+  playBtn.innerText = "Pause";
+};
+const handlePause = (e) => {
+  playBtn.innerText = "Play";
+};
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("play", handlePlay);
+video.addEventListener("pause", handlePause);
