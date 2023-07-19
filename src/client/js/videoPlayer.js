ebde50cee510672f26f0f9a0a33f595803deff35
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-07-19 18:50:01 +0900

    11.3
    11.3까지 영상 시청 후 구현까지 완료함

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,26 +4,34 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
   if (video.paused) video.play();
   else video.pause();
   playBtn.innerText = video.paused ? "Play" : "Pause";
-};
-const handlePlay = (e) => {
-  playBtn.innerText = "Pause";
-};
-const handlePause = (e) => {
-  playBtn.innerText = "Play";
 };
 
 const handleMute = (e) => {
   if (video.muted) video.muted = false;
   else video.muted = true;
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Unmute";
+  }
+  volumeValue = value;
+  video.volume = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("play", handlePlay);
-video.addEventListener("pause", handlePause);
+volumeRange.addEventListener("input", handleVolumeChange);
